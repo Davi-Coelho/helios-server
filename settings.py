e86e61e5984e737c62ffa19c3183e5f2545866ec
Davi-Coelho <davi.cmbarreto@gmail.com>
2025-02-02 16:19:28 -0400

    fix: :bug: media url

--- settings.py
+++ settings.py
@@ -100,12 +100,12 @@ MEDIA_ROOT = '/workspace/helios/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
 STATIC_ROOT = ROOT_PATH + '/sitestatic'
 
